c61b0b9ddd49988c6ad8acb33cb756f5a9e40a49
osiykm <osiykmukola@gmail.com>
2016-03-16 00:07:10 +0200

    init

---
+++ geneticAlgorithm/geneticAlgorithm/Coordinates.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_1_genetic_algoritn
+{/// <summary>
+/// Клас координат 
+/// </summary>
+    class Coordinates
+    {
+        private int x { get; }
+        private int y { get; }
+        Coordinates(int x, int y)
+        {
+
+        }
+        /// <summary>
+        /// Вертає відстань між точками а і b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        static double getLenght(Coordinates a, Coordinates b) 
+        {
+            return Math.Sqrt(Math.Pow(a.x - b.x, 2)+Math.Pow(a.y-b.y, 2));
+            
+        }
+    }
+}
---
+++ geneticAlgorithm/geneticAlgorithm/GeneticAlgorithm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_1_genetic_algoritn
+{
+    class GeneticAlgorithm
+    {
+        /// <summary>
+        /// Инициализация генетического алгоритма
+        /// </summary>
+        /// <param name="maxAges">Максимальное количество поколений </param>
+        /// <param name="pointArray"> Точки</param>
+        /// <param name="mutationChance">Шанс мутаций</param>
+        /// <param name="populationSize количество особей в популяции">количес</param>
+        GeneticAlgorithm(int maxAges, List<Coordinates> pointArray, double mutationChance, int populationSize)
+        {
+
+        }
+    }
+}
