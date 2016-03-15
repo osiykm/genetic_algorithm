4f6853dd7202a638c04e5f8357a5538ecd402676
osiykm <osiykmukola@gmail.com>
2016-03-16 00:15:54 +0200

    Добавлен mutationEnum

--- geneticAlgorithm/geneticAlgorithm/Coordinates.cs
+++ geneticAlgorithm/geneticAlgorithm/Coordinates.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace laba_1_genetic_algoritn
+namespace geneticAlgorithm
 {/// <summary>
 /// Клас координат 
 /// </summary>
--- geneticAlgorithm/geneticAlgorithm/GeneticAlgorithm.cs
+++ geneticAlgorithm/geneticAlgorithm/GeneticAlgorithm.cs
@@ -4,10 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace laba_1_genetic_algoritn
+namespace geneticAlgorithm
 {
+    
     class GeneticAlgorithm
     {
+
+        private int maxAges;
+        private List<Coordinates> pointArray;
+        private double mutationChance;
+        private int populationSize;
+        private List<Mutation> mutation;
         /// <summary>
         /// Инициализация генетического алгоритма
         /// </summary>
@@ -15,9 +22,14 @@ namespace laba_1_genetic_algoritn
         /// <param name="pointArray"> Точки</param>
         /// <param name="mutationChance">Шанс мутаций</param>
         /// <param name="populationSize количество особей в популяции">количес</param>
-        GeneticAlgorithm(int maxAges, List<Coordinates> pointArray, double mutationChance, int populationSize)
+        GeneticAlgorithm(int maxAges, List<Coordinates> pointArray, double mutationChance, int populationSize, List<Mutation> mutation)
         {
-
+            this.maxAges = maxAges;
+            this.pointArray = pointArray;
+            this.mutationChance = mutationChance;
+            this.populationSize = populationSize;
+            this.mutation = mutation;
         }
+
     }
 }
