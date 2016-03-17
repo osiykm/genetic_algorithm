6bcf538a906514d58d812dbb920507c40752486b
osiykm <osiykmukola@gmail.com>
2016-03-17 14:03:27 +0200

    Добавлен циклический кроссовер(CYCLE)

--- geneticAlgorithm/geneticAlgorithm/Form1.cs
+++ geneticAlgorithm/geneticAlgorithm/Form1.cs
@@ -20,14 +20,9 @@ namespace geneticAlgorithm
         
         private void textBox1_Click(object sender, EventArgs e)
         {
-            Chromosome a = new Chromosome() ;
-            Chromosome b = new Chromosome() ;
-            for (int i = 0; i < 10; i++)
-            {
-                a.Add(i);
-                b.Add(9 - i);
-            }
-            label1.Text = a.toString()+"\n" + b.toString() + "\n" + Chromosome.OrderedCrossover(a, b).toString();
+            Chromosome a = new Chromosome() { 1, 4, 8, 6, 3, 2, 7, 5};
+            Chromosome b = new Chromosome() { 1, 7, 5, 4, 2, 3, 6, 8};
+            label1.Text = a.toString()+"\n" + b.toString() + "\n" + Chromosome.CycleCrossover(a, b).toString();
         }
     }
     }
